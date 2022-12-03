81541aac0fb5c478fc432dea3c317d328af688bb
Jeremiaszmacura <jeremiaszmacura99@gmail.com>
2022-12-03 11:32:24 +0100

    backup database switch

--- flaskr/models/db.py
+++ flaskr/models/db.py
@@ -8,8 +8,9 @@ from cryptography.fernet import Fernet
 client = MongoClient(environ.get("DEV_DATABASE_URI", "localhost:27017"))
 db = client.test
 
-client_backup = MongoClient(environ.get("BACKUP_DATABASE_URI2", "localhost:27017"))
-db_backup = client_backup.test
+if environ.get("SET_BACKUP_DATABASE"):
+    client_backup = MongoClient(environ.get("BACKUP_DATABASE_URI2", "localhost:27017"))
+    db_backup = client_backup.test
 
 key: str = environ.get("CRYPTO_KEY", Fernet.generate_key())
 if isinstance(key, str):
